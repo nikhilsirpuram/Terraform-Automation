4a3c2ad9be63c993b96326bc22082d9e42251ab3
nikhilsirpuram <145117227+nikhilsirpuram@users.noreply.github.com>
2023-11-21 22:19:03 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,4 +1,7 @@
-resource "aws_s3_bucket" "Vattu1" {
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "vattu1" # change this
+}
+/*resource "aws_s3_bucket" "Vattu1" {
   bucket_prefix = var.bucket_prefix
   acl = var.acl
   
@@ -7,4 +10,4 @@ resource "aws_s3_bucket" "Vattu1" {
   }
   
   tags = var.tags
-}
+}*/

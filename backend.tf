5874889c0fb709fced11e8d0409b7b9a65d82a2b
nikhilsirpuram <145117227+nikhilsirpuram@users.noreply.github.com>
2023-11-21 22:21:56 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,15 @@
 terraform {
   backend "s3" {
+    bucket = "vattu1"
+    region = "us-east-1"
+    key = "eks/terraform.tfstate"
+  }
+}
+/*terraform {
+  backend "s3" {
     bucket = "mydev-tf-state-bucket-project-terraform-batch-20"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
-}
+}*/
